42a9061437c57df06cfd9f6672faad1575c3aeea
Somsak Sae-Lim <Somsak Sae-Lim>
2017-03-18 16:33:48 +0700

    ตกแต่งหน้าแรก

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,3 +1,15 @@
+const frame = require('ui/frame')
+
 exports.pageLoaded = (args) => {
   let page = args.object
+}
+
+exports.goNewClassroom = () => {
+  let topmost = frame.topmost()
+  topmost.navigate('views/new-classroom/new-classroom')
+}
+
+exports.goJoinClassroom = () => {
+  let topmost = frame.topmost()
+  topmost.navigate('views/join-classroom/join-classroom')
 }
